f9354893e2f2e7450dc43e93bebf8ac3013e86bf
ebenzOW <ebenz@oldwestbury.edu>
2025-10-22 15:42:50 -0400

    Fix CircularBuffer constructor and member variables and added maybe functional changes to push back and pop front

--- CircularBuffer.cpp
+++ CircularBuffer.cpp
@@ -3,32 +3,39 @@
 using namespace std;
 
 class CircularBuffer {
-    
-    private:
-        int* arr;
-        int capacity;
-        int size_;
-        int front;
-        int back;
-        
-    public:
-        CircularBuffer cirbuf (int capacity = 5) : capacity(capacity)  {
-            arr = new int[capacity];
-            size_ = 0;
-            front = 0;
-            back = 0;
-        }
-        
-        void push_back (int a) {
-            arr[size_] = a;
+
+private:
+    int* arr;
+    int capacity;
+    int size;
+    int front;
+    int back;
+
+public:
+    CircularBuffer(int capacity = 5) : capacity(capacity) {
+        arr = new int[capacity];
+        size = 0;
+        front = 0;
+        back = 0;
+    }
+
+    void push_back(int a) {
+        if (size != capacity)
+        {
+            arr[back] = a;
             size++;
             back++;
+            if (back == capacity - 1) back = 0;
         }
-        
-        int pop_front () {
+    }
+
+    int pop_front() {
+        if (size != 0) {
             front++;
             size--;
+            if (front == capacity - 1) front = 0;
         }
+    }
 
 };
 
